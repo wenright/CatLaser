10c12e6a906eb2b49b8c7be9b5c71b42889343f5
wenright <wenright0@gmail.com>
2024-04-14 22:46:11 -0500

    move LethalThings to a soft dependency and fix client laser angles

---
+++ LethalThingsCompatibility.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using LethalThings;
+using UnityEngine;
+
+namespace CatLaser;
+
+public static class LethalThingsCompatibility
+{
+    private static bool? _enabled;
+
+    public static bool enabled
+    {
+        get
+        {
+            if (_enabled == null)
+            {
+                _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("evaisa.lethalthings");
+            }
+            
+            return (bool)_enabled;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    public static bool IsLauncherButDisabled(GrabbableObject laserSource)
+    {
+        return laserSource is RocketLauncher launcher && !launcher.laserPointer.enabled;
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,20 +1,24 @@
-﻿using BepInEx;
+﻿using System.Linq;
+using BepInEx;
+using BepInEx.Bootstrap;
 using CatLaser.Patches;
 using HarmonyLib;
+using UnityEngine;
 
-namespace CatLaser
+namespace CatLaser;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("evaisa.lethalthings", BepInDependency.DependencyFlags.SoftDependency)]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+    private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
         
-        private void Awake()
-        {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        Debug.Log(BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("evaisa.lethalthings"));
             
-            harmony.PatchAll(typeof(PlayerControllerBPatch));
-        }
+        harmony.PatchAll(typeof(PlayerControllerBPatch));
     }
 }
